9f524177b187f54412bdcbd9dfdb53a1e788783e
Nimisha Padole <92087994+NimishaNikhilBorkar@users.noreply.github.com>
2025-08-20 14:16:47 +0530

    Add files via upload

---
+++ Selenium/Project/Activity3.java
@@ -0,0 +1,19 @@
+package project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity3{
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		
+
+		// Open the page
+		driver.get("https://alchemy.hguy.co/jobs");
+		// Print the url of the page
+		System.out.println("Page URL: " + driver.getCurrentUrl());
+		driver.quit();
+	}
+
+}
